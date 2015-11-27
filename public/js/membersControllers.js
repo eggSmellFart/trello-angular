709f40c02e828f8b05da39feb3cb192425c1336b
Markus Offermann <offermann@source-lounge.de>
2015-11-27 09:16:27 +0100

    cards to members changed

--- public/js/membersControllers.js
+++ public/js/membersControllers.js
@@ -62,12 +62,13 @@ trelloControllers.controller('AngularGetBoardsToMembersController',[ '$scope', '
 }]);
 
 trelloControllers.controller('AngularGetCardsToMembersController',[ '$scope', '$http', '$location', function ($scope, $http, $location){ 
-    $scope.cardsToMembers = [];
-    $scope.member = [];
-    $scope.membersCards = [];
+    $scope.cardsToMember = [];
+    // $scope.membersCards = [];
     $scope.cards = [];
     var cardMemberID;
     var cardID;
+    $scope.lists = [];
+
     $scope.loading = false;
     $scope.init = function () {
         $scope.loading = true;
@@ -76,46 +77,39 @@ trelloControllers.controller('AngularGetCardsToMembersController',[ '$scope', '$
         $http.get('http://trello-office.mo/api/members/' + memberID).then(function successCallback(response) {
             console.log('success');
             $scope.members = response.data;
-            console.log('http://trello-office.mo/api/cardsToMembers/'+ $scope.members[0].trello_id);
+            var memberID = $scope.members[0].trello_id;
+            $http.get('http://trello-office.mo/api/cardsToMembers/' + $scope.members[0].trello_id).then(function successCallback (response) {
+                $scope.members.cards = response.data;
+                angular.forEach($scope.members.cards, function(item, index){
+                    // console.log('http://trello-office.mo/api/cards/' + item.cards_trello_id);
+                    $http.get('http://trello-office.mo/api/cardsTrelloID/' + item.cards_trello_id).then(function successCallback (response) {
+                        if(response.data.length > 0) {
+                            $scope.cardsToMember.push(response.data[0]);
+
+                            $http.get('http://trello-office.mo/api/listsTrelloID/' + response.data[0].lists_trello_id).then(function successCallback (response) {
+console.log($scope.cardsToMember);                            
+                            //     if (response.data[0].type == 'ready' && reponse.data[0].display == 1 ) {
+                            //         scope.lists = response.data;
+                            //     }
+                             }, function errorCallback (response) {
+                            //     console.log('error: ', response.data);
+                             })
+// console.log(response.data[0].lists_trello_id);
+                            // var listTrelloID = $scope.cardsToMember.
+                            // $http.get('http://trello-office.mo/api/lists/' + listTrelloID) 
+                            // console.log($scope.members.cards);
+                        }
+                    }, function errorCallback (response) {
+                        console.log('error: ', response.data);
+                    })
+                })
+            }, function errorCallback (response) {
+                console.log('error: ', response.data);
+            }); 
         }, function errorCallback(response) {
-            console.log('error');
+            console.log('error: ', response.data);
         });
-
-//     $scope.init = function () {
-//         $scope.loading = true;
-//         apiURL = $location.path();
-//         memberID = apiURL.split("/")[2]||"Unknown";
-//         $http.get('http://trello-office.mo/api/members/' + memberID).then(function successCallback(response) {
-//             console.log('success');
-//             $scope.members = response.data;
-//         }, function errorCallback(response) {
-//             console.log('error');
-//         });
-//         $http.get('http://trello-office.mo/api' + apiURL).then(function successCallback(response) {
-//             console.log('success');
-//             // response.data =  response.data.splice(0, 500);
-//             $scope.cardsToMembers = response.data;
-// console.log(response.data.length);
-//             angular.forEach($scope.cardsToMembers, function(item, index) {
-//                 if (item.members_trello_id ==  $scope.members[0].trello_id) {
-//                     $http.get('http://trello-office.mo/api/cards/' + item.cards_trello_id).then(function successCallback(response) {
-//                         if (response.data.length > 0) {
-//                             $scope.cards.push(response.data);
-// console.log('http://trello-office.mo/api/cards/' + item.cards_trello_id);
-//                         }
-                        
-//                     }, function successCallback (response) {
-//                         console.log('error: ', response.data);
-//                     })
-//                 }
-                
-//             })
-
-//         }, function errorCallback (response) {
-//             console.log('error');
-//         });
-        
     }
+    // $http.get('http://trello-office.mo/api/')
     $scope.init();
-
 }]);
